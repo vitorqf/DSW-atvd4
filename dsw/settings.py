cb67e212ac25aef1441025becf850775cd3d6627
Vitor Rafael <rqfvitor@gmail.com>
2023-09-14 15:51:58 -0300

    feat: add new app into installed apps

--- dsw/settings.py
+++ dsw/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.app_1'
+    'apps.app_1',
+    'apps.app_2'
 ]
 
 MIDDLEWARE = [
--- dsw/urls.py
+++ dsw/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.app_1.urls'))
+    path('', include('apps.app_1.urls')),
+    path('', include('apps.app_2.urls'))
 ]
